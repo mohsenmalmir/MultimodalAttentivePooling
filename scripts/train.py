21321cdf2ca6689121a3398b51c9fad1205f399d
Mohsen Malmir <mmalmir@eng.ucsd.edu>
2021-01-31 13:01:27 -0500

    decreasing print frequency

--- scripts/train.py
+++ scripts/train.py
@@ -75,7 +75,7 @@ def run(dataset, dataset_args, dataloader, dataloader_args, transforms, transfor
             data["loss"].backward()
             optimizer.step()
             # misclassification
-            if epoch_index%10==0:
+            if epoch_index%100==0:
                 print(epoch_index,data["loss"].item())
                 pred = data["win33"]
                 pred = torch.argmax(pred,dim=1).data.cpu().numpy().reshape(-1)
