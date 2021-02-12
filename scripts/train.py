6ac5cd05967d6ca1f0892d066e3985a0730636b9
Mohsen Malmir <mmalmir@eng.ucsd.edu>
2021-02-11 22:03:05 -0500

    decrease print frequency

--- scripts/train.py
+++ scripts/train.py
@@ -84,7 +84,7 @@ def run(dataset, dataset_args, dataloader, dataloader_args, transforms, transfor
             optimizer.step()
             scheduler.step(epoch + epoch_index / len(dataloader))
             # misclassification
-            if epoch_index%5==0:
+            if epoch_index%500==0:
                 print(epoch, epoch_index,data["loss"].item())
                 pred = data["win21"]
                 # print(pred.shape)
