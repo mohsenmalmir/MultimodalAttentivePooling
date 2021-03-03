fbd258d9129ec784dc461c10c2896ec01c27652c
Mohsen Malmir <mmalmir@eng.ucsd.edu>
2021-03-02 22:50:02 -0500

    switching to conv prediction

--- multimodalattentivepooling/model/tfbaseline.py
+++ multimodalattentivepooling/model/tfbaseline.py
@@ -1,4 +1,5 @@
 from torch.nn import Module, AdaptiveMaxPool1d, Linear,ReLU,ModuleList,ModuleDict,Sequential,Dropout,MultiheadAttention
+from torch.nn import Conv1d
 import torch.nn.functional as F
 from multimodalattentivepooling.model.encoder import SeqEncoder
 from multimodalattentivepooling.model.sequtils import PositionalEncoding
@@ -21,8 +22,10 @@ class Baseline(Module):
         self.vid_enc = torch.nn.Sequential(Linear(vis_dim, vis_dim),ReLU(),Linear(vis_dim, vis_dim))
         self.query_enc = torch.nn.Sequential(Linear(q_dim, q_dim),ReLU(),Linear(q_dim, vis_dim))
         self.vis_dim, self.q_dim = vis_dim, q_dim
-        self.start_pred = Linear(vis_dim, 1)
-        self.end_pred = Linear(vis_dim, 1)
+        # self.start_pred = Linear(vis_dim, 1)
+        # self.end_pred = Linear(vis_dim, 1)
+        self.start_pred = Conv1d(vis_dim, 1, 21, padding=10)
+        self.end_pred = Conv1d(vis_dim, 1, 21, padding=10)
         self.vid_pool = AdaptiveMaxPool1d(pred_len)
         self.q_pool = AdaptiveMaxPool1d(pred_len)
         self.device = None
@@ -50,6 +53,6 @@ class Baseline(Module):
         vis_feats = self.mha(query.transpose(0,1),vis_feats.transpose(0,1),vis_feats.transpose(0,1))[0].transpose(0,1)
         vis_feats = vis_feats + query
         # vis_feats = vis_feats.contiguous().view(vis_feats.shape[0],-1)
-        data[self.start_name] = self.start_pred(vis_feats)
-        data[self.end_name] = self.end_pred(vis_feats)
+        data[self.start_name] = self.start_pred(vis_feats.transpose(1,2)).transpose(1,2)
+        data[self.end_name] = self.end_pred(vis_feats.transpose(1,2)).transpose(1,2)
         return data
