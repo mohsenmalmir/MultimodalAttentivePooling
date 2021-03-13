a5e09532fa758ddd1372dbfc77940a9fa4462594
Mohsen Malmir <mmalmir@eng.ucsd.edu>
2021-03-12 21:56:56 -0500

    added sequence encoder

--- multimodalattentivepooling/model/tfbaseline.py
+++ multimodalattentivepooling/model/tfbaseline.py
@@ -17,15 +17,15 @@ class Baseline(Module):
         # positional encodings of the sequences
         self.vid_pe = PositionalEncoding(vis_dim)
         self.seq_pe = PositionalEncoding(q_dim)
-        # self.vid_enc = SeqEncoder(d_model=vis_dim, d_out=vis_dim)
-        # self.query_enc = SeqEncoder(d_model=q_dim, d_out=vis_dim)
-        self.vid_enc = torch.nn.Sequential(Linear(vis_dim, vis_dim),ReLU(),Linear(vis_dim, vis_dim))
-        self.query_enc = torch.nn.Sequential(Linear(q_dim, q_dim),ReLU(),Linear(q_dim, vis_dim))
+        self.vid_enc = SeqEncoder(d_model=vis_dim, d_out=vis_dim)
+        self.query_enc = SeqEncoder(d_model=q_dim, d_out=vis_dim)
+        # self.vid_enc = torch.nn.Sequential(Linear(vis_dim, vis_dim),ReLU(),Linear(vis_dim, vis_dim))
+        # self.query_enc = torch.nn.Sequential(Linear(q_dim, q_dim),ReLU(),Linear(q_dim, vis_dim))
         self.vis_dim, self.q_dim = vis_dim, q_dim
-        # self.start_pred = Linear(vis_dim, 1)
-        # self.end_pred = Linear(vis_dim, 1)
-        self.start_pred = Conv1d(vis_dim, 1, 21, padding=10)
-        self.end_pred = Conv1d(vis_dim, 1, 21, padding=10)
+        self.start_pred = Linear(vis_dim, 1)
+        self.end_pred = Linear(vis_dim, 1)
+        # self.start_pred = Conv1d(vis_dim, 1, 21, padding=10)
+        # self.end_pred = Conv1d(vis_dim, 1, 21, padding=10)
         self.vid_pool = AdaptiveMaxPool1d(pred_len)
         self.q_pool = AdaptiveMaxPool1d(pred_len)
         self.device = None
@@ -52,7 +52,10 @@ class Baseline(Module):
         # transpose C to dim=1 to apply unfold
         vis_feats = self.mha(query.transpose(0,1),vis_feats.transpose(0,1),vis_feats.transpose(0,1))[0].transpose(0,1)
         vis_feats = vis_feats + query
+        # print(vis_feats.shape)
         # vis_feats = vis_feats.contiguous().view(vis_feats.shape[0],-1)
-        data[self.start_name] = self.start_pred(vis_feats.transpose(1,2)).transpose(1,2)
-        data[self.end_name] = self.end_pred(vis_feats.transpose(1,2)).transpose(1,2)
+        # data[self.start_name] = self.start_pred(vis_feats.transpose(1,2)).transpose(1,2)
+        # data[self.end_name] = self.end_pred(vis_feats.transpose(1,2)).transpose(1,2)
+        data[self.start_name] = self.start_pred(vis_feats).squeeze(1)
+        data[self.end_name] = self.end_pred(vis_feats).squeeze(1)
         return data
